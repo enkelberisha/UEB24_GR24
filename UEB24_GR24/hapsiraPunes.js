44f276d1ad20b3f96e80aba40853e2c8ca54b5bf
Enkel Berisha <99924910+enkelberisha@users.noreply.github.com>
2024-11-24 23:27:28 +0100

    Add files via upload

---
+++ UEB24_GR24/hapsiraPunes.js
@@ -0,0 +1,25 @@
+
+function toggleNavOnScroll() {
+    const nav = document.querySelector('nav');
+    const hero = document.querySelector('.hero');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > hero.offsetHeight - nav.offsetHeight) {
+            nav.classList.add('scrolled');
+        } else {
+            nav.classList.remove('scrolled');
+        }
+    });
+
+}
+document.addEventListener('DOMContentLoaded', toggleNavOnScroll);
+
+function changeValueOnSlider() {
+    const slider = document.getElementById('slider');
+    const rangeValue = document.getElementById('rangeValue');
+    rangeValue.textContent = slider.value;
+
+}
+document.getElementById('slider').addEventListener('input', changeValueOnSlider);
+
+
