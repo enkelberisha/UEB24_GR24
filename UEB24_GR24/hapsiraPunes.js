3597d7067a334ebf7d310dade7ff022883b98695
Enkel Berisha <99924910+enkelberisha@users.noreply.github.com>
2024-12-15 02:00:26 +0100

    plane type update

--- UEB24_GR24/hapsiraPunes.js
+++ UEB24_GR24/hapsiraPunes.js
@@ -22,4 +22,26 @@ function changeValueOnSlider() {
 }
 document.getElementById('slider').addEventListener('input', changeValueOnSlider);
 
+document.addEventListener('DOMContentLoaded', () => {
+    const images = [
+        //shto imazhe per slideshow ketu:
+        'img/slideshow/slide-show-hangar1.jpg',
+        'img/slideshow/slide-show-plane2.avif',
+        'img/slideshow/slide-show-inside3.jpg',
+        'img/slideshow/slide-show-plane4.jpg',
+        '/UEB24_GR24/img/slideshow/sidle-show-plane5.jpg',
+    ];
+    let currentIndex = 0;
+
+    const slideshowImage = document.getElementById('slideshow-image');
+
+    const showNextImage = () => {
+        currentIndex = (currentIndex + 1) % images.length;
+        slideshowImage.src = images[currentIndex];
+    };
+
+   
+    setInterval(showNextImage, 3000);
+});
+
 
