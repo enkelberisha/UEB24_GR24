c49df4000e623ad7944f73585f9bfa5f933a387b
Enkel Berisha <99924910+enkelberisha@users.noreply.github.com>
2025-01-06 23:34:12 +0100

    Revert "plane-types/update #3"

--- UEB24_GR24/js/buy-rent.js
+++ UEB24_GR24/js/buy-rent.js
@@ -16,9 +16,8 @@ function addPlane(type, name) {
 // aeroplanat--lista
 addPlane("Jet", "Learjet 75");
 addPlane("Jet", "Gulfstream G650");
-addPlane("Single engine", "Cessna 180");
-addPlane("Multi-Engine", "Beechcraft 58");
-addPlane("Helicopter", "Airbus AS355NP");
+addPlane("Propeller", "Cessna 172");
+addPlane("Helicopter", "Bell 407");
 
 // mbush dropdown
 window.addEventListener("load", function () {
